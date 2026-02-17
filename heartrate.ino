c6f2a7aa2ebe0780682d7ab1ef4ae54b26cac056
Salwa Majeed <smaje4@uic.edu>
2026-02-16 18:54:39 -0600

    Update heartrate.ino

--- heartrate.ino
+++ heartrate.ino
@@ -23,21 +23,21 @@ void setup(){
   
   Wire.begin();
   int result = bioHub.begin();
-  if (!result)
-    Serial.println("Sensor started!");
-  else
-    Serial.println("Could not communicate with the sensor!!!");
+  //if (!result)
+    //Serial.println("Sensor started!");
+  //else
+    //Serial.println("Could not communicate with the sensor!!!");
 
-  Serial.println("Configuring Sensor...."); 
+  //Serial.println("Configuring Sensor...."); 
   int error = bioHub.configBpm(MODE_ONE); // Configuring just the BPM settings. 
-  if(!error){
-    Serial.println("Sensor configured.");
-  }
-  else {
-    Serial.println("Error configuring sensor.");
-    Serial.print("Error: "); 
-    Serial.println(error); 
-  }
+  //if(!error){
+    //Serial.println("Sensor configured.");
+  //}
+  //else {
+    //Serial.println("Error configuring sensor.");
+    //Serial.print("Error: "); 
+    //Serial.println(error); 
+  //}
   // Data lags a bit behind the sensor, if you're finger is on the sensor when
   // it's being configured this delay will give some time for the data to catch
   // up. 
@@ -49,14 +49,15 @@ void loop(){
     // Information from the readBpm function will be saved to our "body"
     // variable.  
     body = bioHub.readBpm();
-    Serial.print("Heartrate: ");
-    Serial.println(body.heartRate); 
-    Serial.print("Confidence: ");
-    Serial.println(body.confidence); 
-    Serial.print("Oxygen: ");
-    Serial.println(body.oxygen); 
-    Serial.print("Status: ");
-    Serial.println(body.status); 
+    Serial.print("H:");
+    Serial.print(body.heartRate);
+    Serial.print(",C:");
+    Serial.print(body.confidence);
+    Serial.print(",O:");
+    Serial.print(body.oxygen);
+    Serial.println(); 
+    // Serial.print("Status: ");
+    // Serial.println(body.status); 
     // tone(BUZZER, 85);
     delay(250); // Slowing it down, we don't need to break our necks here.
 }
